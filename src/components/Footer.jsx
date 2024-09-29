f2c3cb8f446b3e25e629dc1af1c1886ae8cfbd6e
Akshay Rawat <157730136+AkshayRawat17@users.noreply.github.com>
2024-09-29 17:22:48 +0530

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-      <h4 className="relative bottom-16 left-1">Project By : Akshay Rawat</h4>
+      <h4 className="relative bottom-16 left-1 text-2xl">Project By : Akshay Rawat</h4>
   );
 }
 
