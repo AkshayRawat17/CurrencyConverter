2f7dcd48ae2252fffdc710b4b47538ac61aed2ee
Akshay <rawatakshay603@gmail.com>
2024-09-29 17:07:07 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+      <h4 className="relative bottom-16 left-1">Project By : Akshay Rawat</h4>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+      <h1 className="relative top-16 left-auto" >Currency Converter</h1>
+  );
+}
+
+export default Header;
